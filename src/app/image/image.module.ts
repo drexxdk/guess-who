7fba4455b84c4473015c8c2ff9e9c0493352087d
Frederik NIelsen <draxxdk@gmail.com>
2022-06-20 17:26:59 +0200

    better loading experience + layout shift fix

--- src/app/image/image.module.ts
+++ src/app/image/image.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ImageComponent } from './image.component';
 import { HeadlineModule } from '@portal-app/game/headline/headline.module';
+import { IconModule } from '@portal-app/icon/icon.module';
 
 @NgModule({
   declarations: [ImageComponent],
-  imports: [CommonModule, HeadlineModule],
+  imports: [CommonModule, HeadlineModule, IconModule],
   exports: [ImageComponent],
 })
 export class ImageModule {}
